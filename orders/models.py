26b309f500123fa540969c8a105abcf539589a92
Nahuel <nahuel.94.arrieta@gmail.com>
2024-01-08 10:45:21 -0300

    feat(orders/models): add orders model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from accounts.models import User
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(
+        User, verbose_name=_("order num"), on_delete=models.CASCADE
+    )
+    ##Verificar 
+    price = models.FloatField("total price", null=False, blanck=False)
+    ##
+    send = models.BooleanField(verbose_name=_("send/"), default=False)
+    created_ad = models.DateField(auto_now=False, auto_now_add=True)
