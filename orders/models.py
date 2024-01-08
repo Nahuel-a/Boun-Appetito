457e60238ececbdc8d18fb8c16233123fe571598
Nahuel <nahuel.94.arrieta@gmail.com>
2024-01-08 11:02:17 -0300

    feat(orders/models): add PaymentMethod the order

--- orders/models.py
+++ orders/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 from accounts.models import User
 from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_noop
 
 
 class Order(models.Model):
+    class PaymentMethod(models.IntegerChoices):
+        CASH = 1, gettext_noop("CASH")
+        CARD = 2, gettext_noop("CARD")
+
     user = models.ForeignKey(
         User, verbose_name=_("order num"), on_delete=models.CASCADE
     )
-    ##Verificar 
+    ##Verificar
     price = models.FloatField("total price", null=False, blanck=False)
     ##
     send = models.BooleanField(verbose_name=_("send/"), default=False)
+    payment_method = models.IntegerField(
+        verbose_name=_("Payment method"), choise=PaymentMethod
+    )
     created_ad = models.DateField(auto_now=False, auto_now_add=True)
 
 
@@ -26,3 +34,6 @@ class Pizza(models.Model):
     price = models.FloatField(verbose_name=_("Price of pizza"))
     image = models.ImageField(verbose_name=_("Image of pizza"))
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
