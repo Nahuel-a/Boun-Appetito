3a8ab34c4d1683a635fa8b6d1c1bc02674bd966c
Nahuel <nahuel.94.arrieta@gmail.com>
2024-01-11 10:45:49 -0300

    feat(views/orders): Created basic OrderViewSet, PizzaViewSet and SendViewSet

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import OrderSerializer, PizzaSerializer, SendSerializer
+from .models import Order, Pizza, Send
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class PizzaViewSet(viewsets.ModelViewSet):
+    queryset = Pizza.objects.all()
+    serializer_class = PizzaSerializer
+
+
+class SendViewSet(viewsets.ModelViewSet):
+    queryset = Send.objects.all()
+    serializer_class = SendSerializer
